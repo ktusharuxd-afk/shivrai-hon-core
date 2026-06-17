6fd488ec1b64b6185f58b20d59f0038c8df8aa21
ktusharuxd-afk <ktusharuxd@gmail.com>
2026-06-17 05:18:02 +0000

    Block reward 2000 HON, halving off

--- src/validation.cpp
+++ src/validation.cpp
@@ -1842,7 +1842,7 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 200 * COIN;
+    CAmount nSubsidy = 2000 * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
